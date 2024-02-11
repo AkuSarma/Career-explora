e78058dec8c9efa8120f1d81db16e6748a9c3d1c
Aku Sarma <akusarma1702@gmail.com>
2024-02-11 20:27:58 +0530

    setup of the react router

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,11 @@
+// https://test-career-explora-default-rtdb.firebaseio.com/contactform.json
+
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>Contact</div>
+  )
+}
+
+export default Contact
